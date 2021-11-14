0e864d6a2d8a5095ed0abfe5301e0a0659dc8052
arisa-maru <kawasaki@clood.jp>
2021-11-14 22:05:22 +0900

    top画像変更

--- pages/index.vue
+++ pages/index.vue
@@ -4,10 +4,20 @@
     <About />
     <Menu />
     <Gallery />
+    <div class="div"></div>
   </div>
 </template>
 
 <script>
 export default {};
 </script>
-<style scoped lang="css"></style>
+<style scoped lang="scss">
+$col: #ffe923;
+
+.div {
+  width: 200px;
+  height: 100px;
+
+  background-color: $col;
+}
+</style>
