8c7ddbb59b710529edd1ae4e968ca9b1c783d83a
kawasaki <kawasaki@clood.jp>
2021-12-13 23:45:16 +0900

    menu

--- components/top/Menu.vue
+++ components/top/Menu.vue
@@ -1,45 +1,38 @@
 <template>
-  <div class="">
-    <section id="menu">
-      <span class="slide-in leftAnime toppage-title-block">
+  <div id="menu" class="page-root">
+    <section class="page-block">
+      <img
+        src="@/assets/images/toppage-title_menu.svg"
+        class="seciton-title"
+        alt=""
+      />
+
+      <a href="/public/menu1.html" class="menu-link right">
+        <div class="menu-right">
+          <p class="-header">Menu</p>
+          <p class="">普段の装い</p>
+          <p class="">日常を愉しむ日本髪</p>
+          <p class="">view</p>
+        </div>
         <img
-          src="img/toppage-title_menu.svg"
-          class="toppage-title slide-in_inner leftAnimeInner fadeInTriggerNomal"
+          src="@/assets/images/top_menu_1-frame.svg"
+          class="toppage_menu1-frame"
           alt=""
         />
-      </span>
-
-      <a href="/public/menu1.html" class="toppage_menu">
-        <div class="fadeInTriggerNomal toppage_menu-wrapper">
-          <img src="img/top_menu_1.jpg" class="toppage_menu-img" alt="" />
-          <div class="toppage_menu-title-block menu-right">
-            <p class="toppage_menu-title-header">Menu</p>
-            <p class="toppage_menu-title">普段の装い</p>
-            <p class="toppage_menu-title-note">日常を愉しむ日本髪</p>
-            <p class="toppage_menu-title-view fadeInTriggerNomal">view</p>
-          </div>
-          <img
-            src="img/top_menu_1-frame.svg"
-            class="toppage_menu1-frame"
-            alt=""
-          />
-        </div>
       </a>
-      <a href="/public/menu2.html" class="toppage_menu">
-        <div class="fadeInTriggerNomal toppage_menu-wrapper">
-          <img src="img/top_menu_2.png" class="toppage_menu-img" alt="" />
-          <div class="toppage_menu-title-block menu-left">
-            <p class="toppage_menu-title-header">Menu</p>
-            <p class="toppage_menu-title">普段の装い</p>
-            <p class="toppage_menu-title-note">日常を愉しむ日本髪</p>
-            <p class="toppage_menu-title-view fadeInTriggerNomal">view</p>
-          </div>
-          <img
-            src="img/top_menu_1-frame.svg"
-            class="toppage_menu2-frame"
-            alt=""
-          />
+      <a href="/public/menu2.html" class="menu-link">
+        <img src="@/assets/images/top_menu_2.png" class="menu-img" alt="" />
+        <div class="menu-left">
+          <p class="">Menu</p>
+          <p class="">普段の装い</p>
+          <p class="">日常を愉しむ日本髪</p>
+          <p class="">view</p>
         </div>
+        <img
+          src="@/assets/images/top_menu_1-frame.svg"
+          class="toppage_menu2-frame"
+          alt=""
+        />
       </a>
     </section>
   </div>
@@ -48,4 +41,52 @@
 <script>
 export default {};
 </script>
-<style scoped lang="css"></style>
+<style scoped lang="scss">
+#menu {
+  margin-top: 100px;
+}
+
+.seciton-title {
+  margin: auto;
+  width: 300px;
+  margin-bottom: 20px;
+}
+
+.menu-link {
+  position: relative;
+  transition: 0.3s;
+
+  &:hover {
+    opacity: 0.5;
+  }
+}
+
+.menu-link.right {
+  width: 100%;
+  height: 600px;
+  display: block;
+  background-image: url(@/assets/images/top_menu_1.jpg);
+}
+
+%menu {
+  position: absolute;
+  z-index: 50;
+}
+
+.menu-right {
+  @extend %menu;
+
+  right: 0;
+  bottom: 0;
+  width: 425px;
+  height: 288px;
+  background-color: rgba(255, 255, 255, 0.681);
+}
+
+.toppage_menu1-frame {
+  @extend %menu;
+
+  right: 0;
+  bottom: 0;
+}
+</style>
