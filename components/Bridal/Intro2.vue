ab958e2e2253f3b8679ba819c1af7199f4b3f2d1
arisa-maru <kawasaki@clood.jp>
2022-01-22 18:00:01 +0900

    bridalのページレスポンシブ実装

--- components/Bridal/Intro2.vue
+++ components/Bridal/Intro2.vue
@@ -52,27 +52,43 @@ export default {};
 .intro-box {
   position: relative;
   margin-bottom: 100px;
+
+  @include mq-m {
+    margin-bottom: 15vw;
+    width: 100%;
+    height: 60vh;
+  }
 }
 
 #intro-box1 {
   height: clamp(400px, 40vw, 595px);
   width: clamp(600px, 98vw, 1000px);
+
+  @include mq-m {
+    width: 100%;
+  }
 }
 
 #intro-img1 {
-  position: absolute;
-  right: 0;
+  position: relative;
   bottom: 0;
   width: clamp(400px, 75vw, 800px);
   height: clamp(400px, 40vw, 595px);
   object-fit: cover;
   z-index: 2;
+
+  @include mq-d {
+    left: 20vw;
+  }
+
+  @include mq-m {
+    left: 10vw;
+  }
 }
 
 #text-box1 {
   position: absolute;
   left: 0;
-  top: 20%;
   background-color: rgba(255, 255, 255, 0.9);
   width: clamp(320px, 35vw, 400px);
   height: clamp(200px, 30vw, 330px);
@@ -80,16 +96,31 @@ export default {};
   display: flex;
   align-items: center;
   justify-content: center;
+
+  @include mq-d {
+    top: 20%;
+  }
+
+  @include mq-m {
+    position: absolute;
+    bottom: 0;
+    width: 65vw;
+    height: 40vw;
+  }
 }
 
 /* intro2 --------------------------------------------*/
 #intro-box2 {
   height: clamp(400px, 40vw, 595px);
   width: clamp(600px, 98vw, 1000px);
+
+  @include mq-m {
+    width: 100%;
+  }
 }
 
 #intro-img2 {
-  position: absolute;
+  position: relative;
   left: 0;
   bottom: 0;
   width: clamp(400px, 75vw, 800px);
@@ -101,7 +132,6 @@ export default {};
 #text-box2 {
   position: absolute;
   right: 0;
-  top: 20%;
   background-color: rgba(255, 255, 255, 0.9);
   width: clamp(320px, 35vw, 400px);
   height: clamp(200px, 30vw, 330px);
@@ -109,28 +139,49 @@ export default {};
   display: flex;
   align-items: center;
   justify-content: center;
+
+  @include mq-d {
+    top: 20%;
+  }
+
+  @include mq-m {
+    position: absolute;
+    bottom: 0;
+    width: 65vw;
+    height: 40vw;
+  }
 }
 
 /* intro3 --------------------------------------------*/
 #intro-box3 {
   height: clamp(400px, 40vw, 595px);
-  width: clamp(600px, 98vw, 3000px);
+  width: clamp(600px, 98vw, 1000px);
+
+  @include mq-m {
+    width: 100%;
+  }
 }
 
 #intro-img3 {
-  position: absolute;
-  right: 0;
+  position: relative;
   bottom: 0;
   width: clamp(400px, 75vw, 800px);
   height: clamp(400px, 40vw, 595px);
   object-fit: cover;
   z-index: 2;
+
+  @include mq-d {
+    left: 20vw;
+  }
+
+  @include mq-m {
+    left: 10vw;
+  }
 }
 
 #text-box3 {
   position: absolute;
   left: 0;
-  top: 20%;
   background-color: rgba(255, 255, 255, 0.9);
   width: clamp(320px, 35vw, 400px);
   height: clamp(200px, 30vw, 330px);
@@ -138,16 +189,31 @@ export default {};
   display: flex;
   align-items: center;
   justify-content: center;
+
+  @include mq-d {
+    top: 20%;
+  }
+
+  @include mq-m {
+    position: absolute;
+    bottom: 0;
+    width: 65vw;
+    height: 40vw;
+  }
 }
 
 /* intro4 --------------------------------------------*/
 #intro-box4 {
   height: clamp(400px, 40vw, 595px);
   width: clamp(600px, 98vw, 1000px);
+
+  @include mq-m {
+    width: 100%;
+  }
 }
 
 #intro-img4 {
-  position: absolute;
+  position: relative;
   left: 0;
   bottom: 0;
   width: clamp(400px, 75vw, 800px);
@@ -159,7 +225,6 @@ export default {};
 #text-box4 {
   position: absolute;
   right: 0;
-  top: 20%;
   background-color: rgba(255, 255, 255, 0.9);
   width: clamp(320px, 35vw, 400px);
   height: clamp(200px, 30vw, 330px);
@@ -167,5 +232,16 @@ export default {};
   display: flex;
   align-items: center;
   justify-content: center;
+
+  @include mq-d {
+    top: 20%;
+  }
+
+  @include mq-m {
+    position: absolute;
+    bottom: 0;
+    width: 65vw;
+    height: 40vw;
+  }
 }
 </style>
