f7ba7d2593ca6b32006994fe66b445bba1a01db3
kawasaki <kawasaki@clood.jp>
2022-03-29 13:08:37 +0900

    thanksページ修正

--- pages/thanks.vue
+++ pages/thanks.vue
@@ -1,8 +1,28 @@
 <template>
-  <div class="thanks">お問い合わせありがとうございました</div>
+  <div class="thanks">
+    <p class="thanks-header">お問い合わせありがとうございました</p>
+    <p class="thanks-note">
+      内容を確認後、数日中に返信いたしますので暫くお待ちいただきますようお願い申し上げます
+    </p>
+
+    <Nuxt-link to="/" class="nomal-btn"
+      ><span>トップページに戻る</span></Nuxt-link
+    >
+  </div>
 </template>
 
 <script>
 export default {};
 </script>
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.thanks {
+  display: flex;
+  flex-direction: column;
+  gap: 50px;
+  align-items: center;
+}
+
+.thanks-header {
+  font-size: 34px;
+}
+</style>
